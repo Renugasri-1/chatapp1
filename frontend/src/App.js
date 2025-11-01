f11ce370bd16d9186802e2d2b61ad8bdc49dbc3f
Renugasri-1 <renusri1505@gmail.com>
2025-11-01 23:52:02 +0530

    "initial"

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+
+import './App.css';
+import Homepage from './pages/Homepage';
+import ChatPage from './pages/ChatPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+     <Route path="/" element={<Homepage />} />
+      <Route path="/chats" element={<ChatPage />} /> 
+       </Routes>
+    </div>
+  
+    
+  );
+}
+
+export default App;
